bec8acc7646445edf88c70e108ef7e90fb8e9656
arenneu <jisusjy@naver.com>
2021-08-03 14:39:35 +0900

    0803_GNB메뉴완료

--- 풀이/script/script.js
+++ 풀이/script/script.js
@@ -1,6 +1,8 @@
 $(function () {
-
-
-
-
+    // GNB 메뉴 blink (깜박임 효과) 시작
+    setInterval(fnBlink, 550);
+    function fnBlink(){
+    $("#blinkStr").toggle();
+    // GNB 메뉴 blink (깜박임 효과) 끝
+}
 });
