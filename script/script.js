9e49fc60f1be29aca03abf73f562cec447496215
arenneu <jisusjy@naver.com>
2021-08-04 15:47:31 +0900

    0804_main&footer작업중

--- script/script.js
+++ script/script.js
@@ -1,11 +1,6 @@
 setInterval (fnShow,1000);
 function fnShow(){
 $(function(){
-    $("#mainMenu li:last-child span").show(function(){
-        $(this).css({"visibility":"hidden"});
+    $("#mainMenu li:last-child span").toggle();
     });
-    $("#mainMenu li:last-child span").hide(function(){
-        $(this).css({"visibility":"visible"});
-    });
-});
 }
